bc345bbac579ed54fdf28a7f7390cdd20406f4ab
Gabriel <Jimenezg905@gmail.com>
2020-10-07 08:50:44 -0400

    Menu 7 : Creado

--- TestProgram/Main_Manu.cs
+++ TestProgram/Main_Manu.cs
@@ -7,6 +7,7 @@ using TestProgram.Menu3;
 using TestProgram.Menu4;
 using TestProgram.Menu5;
 using TestProgram.Menu6;
+using TestProgram.Menu7;
 
 namespace TestProgram
 {
@@ -19,6 +20,7 @@ namespace TestProgram
 		Menu_4 menu4 = new Menu_4();
 		Menu_5 menu5 = new Menu_5();
 		Menu_6 menu6 = new Menu_6();
+		Menu_7 menu7 = new Menu_7();
 		public void Operaciones() 
 		{
 			string linea;
@@ -37,6 +39,7 @@ namespace TestProgram
 				Console.WriteLine("*   [4] -- IR Al Menu 4                                         *");
 				Console.WriteLine("*   [5] -- IR Al Menu 5                                         *");
 				Console.WriteLine("*   [6] -- IR Al Menu 6                                         *");
+				Console.WriteLine("*   [7] -- IR Al Menu 7                                         *");
 				Console.WriteLine("*                                                               *");
 				Console.WriteLine("*****************************************************************");
 				Console.WriteLine("*  [99] -- SALIR                                                *");
@@ -88,6 +91,7 @@ namespace TestProgram
 						Console.Clear();
 						Console.WriteLine("Has Seleccionado el MENU 7");
 						Console.ReadLine();
+						menu7.Operaciones();
 						break;
 					case 8:
 						Console.Clear();
